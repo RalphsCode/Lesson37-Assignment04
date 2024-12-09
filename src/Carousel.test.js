a1204b7ee4c2e01ada23435780f094262bbd82fa
RalphsCode <RalphsCode@gmail.com>
2024-12-09 12:00:41 -0800

    Left Arrow test written, and bug fixed.

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -21,6 +21,7 @@ it("works when you click on the right arrow", function() {
       title="images for testing"
     />
   );
+
   // expect the first image to show, but not the second
   expect(
     container.querySelector('img[alt="testing image 1"]')
@@ -40,4 +41,32 @@ it("works when you click on the right arrow", function() {
   expect(
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
+});
+
+
+// Test the left arrow functionality
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
 });
